e844ba8dafc44047f4809f954c97babb7c69620e
alvisLu <alvislu@mapacode.tw>
2016-12-16 16:02:15 +0800

    Alvis: 移除與 Agent ping/pong 通訊的動作。

--- adapter/example.js
+++ adapter/example.js
@@ -1,11 +1,12 @@
-// node version: 0.1.38
+// node version: 0.10.38
 var net = require('net');
-var host = "localhost"; //"192.168.1.124";
+var host = "192.168.7.1"; //"localhost"; //
 var port = 7878;
 
 console.log('Waitting for connection');
 
 var server = net.createServer(function(c) { 
+    /*
     c.on('data',function(data) {
         console.log('Receive:' + data);
         var regExp = new RegExp(/\* PING/);
@@ -14,38 +15,37 @@ var server = net.createServer(function(c) {
             c.write('* PONG 10000');  
             console.log('Responding with PONG');
         }
+    });
+    */
+    console.log("Agent connected");
+    console.log("Type an adapter feed string in the followning format:");
+    console.log("key|value");
+    console.log("key|value|key1|value1...");
+    
+    // Readline 
+    var readline = require('readline');
+    var rl = readline.createInterface(process.stdin, process.stdout);
 
-        console.log("Agent connected");
-        console.log("Type an adapter feed string in the followning format:");
-        console.log("key|value");
-        console.log("key|value|key1|value1...");
-        
-         // Readline 
-        var readline = require('readline');
-        var rl = readline.createInterface(process.stdin, process.stdout);
-
-        rl.setPrompt(">");
+    rl.setPrompt(">");
+    rl.prompt();
+    
+    rl.on('line', function(line) {
+        console.log("|" + line + "\n");
+        c.write("|" + line + "\n");
         rl.prompt();
-        
-        rl.on('line', function(line) {
-            console.log("|" + line + "\n");
-            c.write("|" + line + "\n");
-            rl.prompt();
-        }).on('close', function() {
-            console.log('Adapter server closed');
-            process.exit(0);
-        });
+    }).on('close', function() {
+        console.log('Adapter server closed');
+        process.exit(0);
     });
 
     c.on('error', function(error){
-        //console.log(error);
         if (error.code == 'EPIPE') {
             console.log('Agent closed. Waiting for conncetion...');
         }
     });
     c.on('close', function(){
-        //console.log("");
-        //console.log("Agent closed");
+        console.log("");
+        console.log("Agent closed");
     });
 });
 
